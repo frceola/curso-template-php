55e11087af7ad354c110b143aabad5e268109b41
frceola <fabioceola@gmail.com>
2024-04-24 16:15:30 -0300

    How to Output Buffer Work

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
 
-    echo 'home';
+    $data = [
+                'name' => 'Fabio',
+                'age'  => '34' 
+            ];
+
+    ob_start();
+
+    extract($data);
+
+    require 'home.php';
+
+    $content = ob_get_contents();
+
+    ob_end_clean();
+
+    var_dump($content);
